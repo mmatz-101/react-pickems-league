be5bb97463e6e0097e3d780f8b1f6750f1c020a2
Myles <utmaddog@gmail.com>
2024-06-15 14:01:05 -0500

    6.15 merge

--- frontend-app/app/user/dashboard/page.tsx
+++ frontend-app/app/user/dashboard/page.tsx
@@ -5,6 +5,12 @@ import { currentDataType } from "@/server/actions/admin/helpers/current-data";
 import { gameType } from "@/server/actions/picks/helpers/game-data";
 import { pickType } from "@/server/actions/picks/helpers/pick-data";
 import { redirect } from "next/navigation";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 interface pickTypeQuery extends pickType {
   expand: { game: gameType };
@@ -16,25 +22,38 @@ export default async function DashboardPage() {
   const picks: pickTypeQuery[] = await pb.collection("picks").getFullList({
     filter: `user="${pb.authStore.model!.id}"`,
     expand: "game",
-
-  })
+  });
 
   const currentData: currentDataType = await pb
     .collection("current")
     .getFirstListItem("");
-  
-  
+
   if (pb.authStore.isValid) {
     return (
       <div>
         <h1>User Dashboard</h1>
         <p>{pb.authStore.model!.first_name}</p>
         <div className="container mx-auto py-10 sm:block md:hidden">
-        <DataTable columns={mobileColumns} data={picks} />
+          <DataTable columns={mobileColumns} data={picks} />
         </div>
         <div className="container mx-auto py-10 hidden md:block">
-        <DataTable columns={columns} data={picks} />
+          <DataTable columns={columns} data={picks} />
         </div>
+
+        <Accordion
+          className="max-w-5xl justify-center"
+          type="single"
+          collapsible
+        >
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Week 1</AccordionTrigger>
+            <AccordionContent>
+              <div className="container mx-auto py-10">
+                <DataTable columns={columns} data={picks} />
+              </div>
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </div>
     );
   }
