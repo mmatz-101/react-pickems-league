43511111b0c24603b69166924cf5caf33defdf05
mmatz-101 <myles.d.matz@gmail.com>
2024-08-20 21:48:47 -0500

    updated path for .env

--- backend-app/main.go
+++ backend-app/main.go
@@ -18,7 +18,7 @@ func main() {
 	app := pocketbase.New()
 
 	// Get env variables
-	err := godotenv.Load()
+	err := godotenv.Load("./.env")
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
