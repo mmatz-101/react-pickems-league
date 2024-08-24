39087446bc1f1ca7f17a79e6985af4814aba6e9c
mmatz-101 <myles.d.matz@gmail.com>
2024-08-24 11:40:49 -0500

    password length must be 8 characters or longer

--- frontend-app/schema/login-schema.ts
+++ frontend-app/schema/login-schema.ts
@@ -1,6 +1,7 @@
-import {z} from 'zod';
+import { z } from "zod";
 
 export const LoginSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(6).max(20),
+  email: z.string().email(),
+  password: z.string().min(8).max(20),
 });
+
--- frontend-app/schema/signup-schema.ts
+++ frontend-app/schema/signup-schema.ts
@@ -1,8 +1,8 @@
-import {z} from 'zod'
+import { z } from "zod";
 
 export const SignupSchema = z.object({
-    firstName: z.string().min(2).max(20),
-    lastName: z.string().min(2).max(20),
-    email: z.string().email(),
-    password: z.string().min(6).max(20),
-})
+  firstName: z.string().min(2).max(20),
+  lastName: z.string().min(2).max(20),
+  email: z.string().email(),
+  password: z.string().min(8).max(20),
+});
